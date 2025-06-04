b3ba11ee6c7b25172f80612ee294eface32f7c13
Rafael Morales <116736888+Rafaelmorales1982@users.noreply.github.com>
2025-06-04 10:10:51 -0300

    teste

---
+++ script.js
@@ -0,0 +1,8 @@
+const nome = document.getElementById("nome");
+let nomeCompleto = prompt("Digite seu nome");
+if (nomeCompleto == "" || nomeCompleto == null) {
+    nomeCompleto = "Visitante"
+  nome.innerHTML = nomeCompleto;
+} else {
+  nome.innerHTML = nomeCompleto;
+}
