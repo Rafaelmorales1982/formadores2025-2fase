4c2462148bd173b3b2138374ec66808178bae8c3
Rafael Morales <116736888+Rafaelmorales1982@users.noreply.github.com>
2025-06-25 10:34:12 -0300

    Update função

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const nome = document.getElementById("nome");
-let nomeCompleto = prompt("Digite seu nome");
-if (nomeCompleto == "" || nomeCompleto == null) {
-    nomeCompleto = "Visitante"
-  nome.innerHTML = nomeCompleto;
-} else {
-  nome.innerHTML = nomeCompleto;
+function inserirNome() {
+  const nome = document.getElementById("nome");
+
+  let nomeCompleto = prompt("Digite seu nome");
+
+  if (nomeCompleto == "" || nomeCompleto == null) {
+    nomeCompleto = "Visitante";
+    nome.innerHTML = nomeCompleto;
+  } else {
+    nome.innerHTML = nomeCompleto;
+  }
 }
+inserirNome();
